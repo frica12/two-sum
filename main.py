8bdfd9d3f002af5566f397f66081ca5e6f9f160b
frica12 <fricaa@naver.com>
2023-03-09 19:40:34 +0900

    update

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def twoSum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if(nums[i] + nums[j] == target):
-                print(f"[{i},{j}]")
-    return
+                # print(f"[{i},{j}]")
+                ans = [i, j]
+    return ans
